3d41c3a2260b0620df5ac441f1b449b3360a0dd8
dnynurakhmad99 <80521202+dnynurakhmad99@users.noreply.github.com>
2022-07-02 22:34:12 +0700

    Add files via upload

---
+++ UTS/Biodata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace UTS
+{
+    class Biodata
+    {
+        private BiodataLengkap lengkap;
+        public Biodata(BiodataLengkap lengkap)
+        {
+            this.lengkap = lengkap;
+        }
+
+        public void isiData()
+        {
+            Thread t1 = new Thread(dataUmum);
+            Thread t2 = new Thread(dataKampus);
+            t1.Start();
+            t2.Start();
+        }
+
+        private void dataUmum()
+        {
+            this.lengkap.namaLengkap(" + Nama Lengkap         : Danny Nurakhmad");
+            this.lengkap.ttLahir(" + Tempat Tanggal Lahir : Yogyakarta, 25 Oktober 1999");
+            this.lengkap.alamatRumah(" + Alamat Rumah         : Demblaksari, RT. 05, Baturetno,\n" + 
+                "                          Banguntapan, Bantul, D.I Yogyakarta");
+        }
+
+        private void dataKampus()
+        {
+            this.lengkap.namaKampus();
+            this.lengkap.namaProdi();
+            this.lengkap.nomorInduk();
+        }
+    }
+}
---
+++ UTS/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|------------------------------------------------------------|");
+            Console.WriteLine("|                       Biodata Lengkap                      |");
+            Console.WriteLine("|------------------------------------------------------------|");
+            Biodata biodata = new Biodata(new BiodataLengkap());
+            biodata.isiData();
+        }
+    }
+
+    class BiodataLengkap
+    {
+        public void namaLengkap(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ttLahir(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void alamatRumah(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void namaKampus()
+        {
+            Console.WriteLine(" + Kampus               : Universitas Amikom Yogyakarta");
+        }
+
+        public void namaProdi()
+        {
+            Console.WriteLine(" + Prodi                : BC-Informatics");
+        }
+
+        public void nomorInduk()
+        {
+            Console.WriteLine(" + NIM                  : 19.61.0158");
+        }
+    }
+}
